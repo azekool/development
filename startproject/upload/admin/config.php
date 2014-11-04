31863c3f588e310a396a8166b8005fabd9517fbb
azekool <aze_kool@hotmail.com>
2014-11-04 19:34:38 +0100

    starting my startproject

---
+++ startproject/upload/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:81/Development/startproject/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost:81/Development/startproject/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:81/Development/startproject/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost:81/Development/startproject/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\Development\startproject\upload/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\Development\startproject\upload/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\Development\startproject\upload/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\Development\startproject\upload/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\Development\startproject\upload/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\Development\startproject\upload/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\Development\startproject\upload/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\Development\startproject\upload/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\Development\startproject\upload/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\Development\startproject\upload/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\Development\startproject\upload/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\Development\startproject\upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'startproject');
+define('DB_PREFIX', 'oc_');
